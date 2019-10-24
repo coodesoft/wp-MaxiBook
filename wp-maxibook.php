5bc6e7dfff30bf3cf81747eb1a8782f2e76b0619
debian <lucho.2012.tandil@gmail.com>
2019-10-24 18:20:27 -0300

    [DEV] Ya se traen las cosas con el curl, ahora falta eliminar los tags de inclusión de estilos y scripts y hacer el wrapper de las peticiones

--- wp-maxibook.php
+++ wp-maxibook.php
@@ -12,13 +12,20 @@ License: GPL2
 
 function wp_mxibook_short_code($attrs){
   $id = $attrs['id'];
-  return '<iframe id="reserva" width="500" height="300" frameborder="0" src="http://book.maxibook.com.ar/index.php?horde='.$id.'" allowfullscreen></iframe>';
+  $html_book = file_get_contents('http://book.maxibook.com.ar/index.php?horde='.$id);
+  $html_cont = $html_book;
+  $html_cont .= '<script src="'.plugin_dir_url(__FILE__).'js/jquery-1.7.2.min.js'.'"></script>';
+  $html_cont .= '<script src="'.plugin_dir_url(__FILE__).'js/jquery.tooltipster.min.js'.'"></script>';
+  $html_cont .= '<link rel="stylesheet" type="text/css" href="'.plugin_dir_url(__FILE__).'css/maxbook-estilo.css'.'" media="screen" />';
+  $html_cont .= '<link rel="stylesheet" type="text/css" href="'.plugin_dir_url(__FILE__).'css/tooltipster.css'.'" media="screen" />';
+
+  return '<div id="reserva" width="500" height="300" allowfullscreen>'.$html_cont.'</div>';
 }
 add_shortcode('maxibook_form', 'wp_mxibook_short_code');
 
 function wp_mxibook_on_activate()
 {
-    //add_option('mi_opcion',255,'','yes');
 }
 register_activation_hook(__FILE__,'wp_mxibook_on_activate');
+
 ?>
