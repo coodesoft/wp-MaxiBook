2a74a7cc6588ba6765d0da01cb821209e195fe37
usuario <lucho.2012.tandil@gmail.com>
2019-10-29 15:38:33 -0300

    [DEV] Plugin funcional, todos los 'wrappers' de peticiones funcionales, falta arreglar el tema de la seleccion de idiomas

--- wp-maxibook.php
+++ wp-maxibook.php
@@ -9,6 +9,8 @@ Author URI:
 License: GPL2
 */
 
+//[MODIFICAR] Este código se puede optimizar, pero se trata de una solución provisoria
+
 function wp_mxibook_get_posts_string(){
   $post_val = '';
 
@@ -25,24 +27,46 @@ function handle_mxibook_requests(){
   }
 
   if ($_GET['accion'] === 'refrescarHora'){
+    $ch = curl_init('http://book.maxibook.com.ar/index.php?accion=refrescarHora');
+    curl_setopt($ch, CURLOPT_POST, TRUE);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, wp_mxibook_get_posts_string());
+    curl_setopt($ch, CURLOPT_HTTPHEADER, ["Cookie: PHPSESSID=".strip_tags($_COOKIE['PHPSESSID'])]);
+
+    $html = curl_exec($ch);
+    $html = wp_mxibook_replace_img($html);
+    echo $html;
     die();
   }
 
   if ($_GET['accion'] === 'refrescarCombos'){
+    $ch = curl_init('http://book.maxibook.com.ar/index.php?accion=refrescarCombos');
+    curl_setopt($ch, CURLOPT_POST, TRUE);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, wp_mxibook_get_posts_string());
+    curl_setopt($ch, CURLOPT_HTTPHEADER, ["Cookie: PHPSESSID=".strip_tags($_COOKIE['PHPSESSID'])]);
+
+    $html = curl_exec($ch);
+    $html = wp_mxibook_replace_img($html);
+    echo $html;
     die();
   }
 
   if ($_GET['accion'] === 'getTraduccion'){
+    $tr = strip_tags($_POST['tr']);
+    echo wp_mxibook_get_traduccion_html($tr);
     die();
   }
 
   if ($_GET['accion'] === 'prereserva'){
     $ch = curl_init('http://book.maxibook.com.ar/index.php?accion=prereserva');
     curl_setopt($ch, CURLOPT_POST, TRUE);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     curl_setopt($ch, CURLOPT_POSTFIELDS, wp_mxibook_get_posts_string());
     curl_setopt($ch, CURLOPT_HTTPHEADER, ["Cookie: PHPSESSID=".strip_tags($_COOKIE['PHPSESSID'])]);
 
-    $html = curl_exec($ch).wp_mxibook_get_html_styles();
+    $html = curl_exec($ch);
+    $html .= wp_mxibook_get_html_styles();
     $html = str_replace(["index.php?"],['?id='.$_GET['id'].'&'],$html);
     echo $html;
     die();
@@ -61,7 +85,6 @@ function handle_mxibook_requests(){
     curl_setopt($ch, CURLOPT_POSTFIELDS,  $_POST);
     curl_setopt($ch, CURLOPT_HTTPHEADER, ["Cookie: PHPSESSID=".strip_tags($_COOKIE['PHPSESSID'])]);
     $html = curl_exec($ch);
-    $html = str_replace(["index.php?"],['?id='.$_GET['id'].'&'],$html);
     echo $html.wp_mxibook_get_html_styles();
     die();
   }
@@ -71,6 +94,34 @@ function wp_mxibook_get_html_styles(){
   $url_plugin = plugin_dir_url(__FILE__);
   $html_cont =  '<link rel="stylesheet" type="text/css" href="'.$url_plugin.'css/tooltipster.css'.'" media="screen" />';
   $html_cont .= '<link rel="stylesheet" type="text/css" href="'.$url_plugin.'css/maxbook-estilo.css'.'" media="screen" />';
+  return $html_cont;
+}
+
+function wp_mxibook_replace_img($html_cont){
+  $url_plugin = plugin_dir_url(__FILE__);
+  return str_replace(["imagenes/load_zona.GIF", "imagenes/sombra_lateral.png", "imagenes/cubiertos_blanco.png"],[$url_plugin.'img/load_zona.GIF',$url_plugin.'img/sombra_lateral.png',$url_plugin.'img/cubiertos_blanco.png'],$html_cont);
+}
+
+function wp_mxibook_get_traduccion_html($tr){
+  $curl       = curl_init('http://book.maxibook.com.ar/index.php?accion=getTraduccion');
+  curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+  curl_setopt($curl, CURLOPT_HTTPHEADER, ["Cookie: PHPSESSID=".strip_tags($_COOKIE['PHPSESSID'])]);
+  curl_setopt($curl, CURLOPT_HEADER, 1);
+  $html_book = curl_exec($curl);
+
+  $html_book  = substr($html_book, strpos($html_book, '<'));
+  $url_plugin = plugin_dir_url(__FILE__);
+
+  $html_cont = str_replace(["index.php?","<script language='javascript' type='text/javascript' src='lib/js/jquery-1.7.2.min.js'></script>",
+                            "<script src='lib/js/jquery.tooltipster.min.js'></script>",
+                            "<link rel='stylesheet' type='text/css' href='lib/estilo.css'>",
+                            "<link rel='stylesheet' type='text/css' href='lib/js/tooltipster.css'>"],['?id='.$id.'&','','','',''],$html_book);
+
+  $html_cont = wp_mxibook_replace_img($html_cont);
+
+  $html_cont  = '<script src="'.$url_plugin.'js/jquery-1.7.2.min.js'.'"></script>'.'<script src="'.$url_plugin.'js/jquery.tooltipster.min.js'.'"></script>'.$html_cont;
+  $html_cont .= wp_mxibook_get_html_styles();
+
   return $html_cont;
 }
 
@@ -94,15 +145,11 @@ function wp_mxibook_get_init_html($id){
   $html_book  = substr($html_book, strpos($html_book, '<'));
   $url_plugin = plugin_dir_url(__FILE__);
 
-  $html_cont = str_replace(["<script language='javascript' type='text/javascript' src='lib/js/jquery-1.7.2.min.js'></script>",
+  $html_cont = str_replace(["index.php?","<script language='javascript' type='text/javascript' src='lib/js/jquery-1.7.2.min.js'></script>",
                             "<script src='lib/js/jquery.tooltipster.min.js'></script>",
                             "<link rel='stylesheet' type='text/css' href='lib/estilo.css'>",
-                            "<link rel='stylesheet' type='text/css' href='lib/js/tooltipster.css'>"],['','','',''],$html_book);
-
-  $html_cont = str_replace(["index.php?",
-                            "imagenes/load_zona.GIF",
-                            "imagenes/sombra_lateral.png",
-                            "imagenes/cubiertos_blanco.png"],['?id='.$id.'&',$url_plugin.'img/load_zona.GIF',$url_plugin.'img/sombra_lateral.png',$url_plugin.'img/cubiertos_blanco.png'],$html_cont);
+                            "<link rel='stylesheet' type='text/css' href='lib/js/tooltipster.css'>"],['?id='.$id.'&','','','',''],$html_book);
+  $html_cont = wp_mxibook_replace_img($html_cont);
 
   $html_cont  = '<script src="'.$url_plugin.'js/jquery-1.7.2.min.js'.'"></script>'.'<script src="'.$url_plugin.'js/jquery.tooltipster.min.js'.'"></script>'.$html_cont;
   $html_cont .= wp_mxibook_get_html_styles();
